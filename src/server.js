4798d089497387fdb648ff200b65c94305e86dc5
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2022-01-09 01:59:33 +0200

    make it better

--- src/server.js
+++ src/server.js
@@ -49,7 +49,7 @@ async function start() {
     if(process.env.NODE_ENV == "production"){
         app.use(express.static('build'));
         app.get('*', (req, res) => {
-            req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+            res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
         });
     }
 
