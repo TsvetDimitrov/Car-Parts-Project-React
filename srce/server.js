b8e46972a2eb23f5083f235224c97fe8128644c2
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2022-01-09 03:29:04 +0200

    test message

--- srce/server.js
+++ srce/server.js
@@ -47,7 +47,7 @@ async function start() {
     app.use(cors());
 
     if(process.env.NODE_ENV == "production"){
-        app.use(express.static('client/build'));
+        app.use(express.static(__dirname + 'client/build'));
         app.get('*', (req, res) => {
             res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
         });
