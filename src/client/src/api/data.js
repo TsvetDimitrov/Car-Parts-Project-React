4765d2b2ea4bc8752ea27493c8c812e81211b880
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2022-01-12 21:48:30 +0200

    request paths starting with /api/...

--- src/client/src/api/data.js
+++ src/client/src/api/data.js
@@ -12,66 +12,66 @@ export const logout = api.logout;
 
 //Facebook login: 
 export async function facebookLogin() {
-    const result = await api.get(host + '/api/auth/facebook');
+    const result = await api.get('/api/auth/facebook');
     return result;
 }
 
 export async function isUserAdmin() {
-    const result = await api.get(host + '/api/create');
+    const result = await api.get('/api/create');
     console.log(result);
     return result;
 }
 
 export async function createPart(category, type, brand, model, yearFrom, yearTo, engineType, partColor, imageUrl, condition, title, price) {
-    const result = await api.post(host + '/api/parts/create', { category, type, brand, model, yearFrom, yearTo, engineType, partColor, imageUrl, condition, title, price });
+    const result = await api.post('/api/parts/create', { category, type, brand, model, yearFrom, yearTo, engineType, partColor, imageUrl, condition, title, price });
     return result;
 }
 
 export async function getNavigationSystems() {
-    const result = await api.get(host + '/api/parts/navigationSystems');
+    const result = await api.get('/api/parts/navigationSystems');
     return result;
 }
 
 export async function getProductById(id) {
-    return await api.get(host + '/api/parts/' + id);
+    return await api.get('/api/parts/' + id);
 }
 
 export async function getAllProducts() {
-    return await api.get(host + '/api/parts');
+    return await api.get('/api/parts');
 }
 
 export async function addProductToCart(id) {
-    return await api.post(host + '/api/parts/add/' + id);
+    return await api.post('/api/parts/add/' + id);
 }
 
 export async function getPartsByType(partsType) {
-    return await api.get(host + '/api/parts/getPartsByType/' + partsType);
+    return await api.get('/api/parts/getPartsByType/' + partsType);
 }
 
 export async function deletePartById(partId) {
-    return await api.del(host + '/api/parts/delete/' + partId);
+    return await api.del('/api/parts/delete/' + partId);
 }
 
 export async function editPartById(category, type, brand, model, yearFrom, yearTo, engineType, partColor, imageUrl, condition, title, price, partId) {
-    return await api.put(host + '/api/parts/edit/' + partId, { category, type, brand, model, yearFrom, yearTo, engineType, partColor, imageUrl, condition, title, price });
+    return await api.put('/api/parts/edit/' + partId, { category, type, brand, model, yearFrom, yearTo, engineType, partColor, imageUrl, condition, title, price });
 }
 
 export async function getUserCartProducts() {
-    return await api.get(host + '/api/parts/cart');
+    return await api.get('/api/parts/cart');
 }
 
 export async function removeProductFromCart(id) {
-    return await api.post(host + '/api/parts/cart/delete/' + id);
+    return await api.post('/api/parts/cart/delete/' + id);
 }
 
 export async function getProductsByBrand(brand) {
-    return await api.get(host + '/api/parts/getPartsByBrand/' + brand);
+    return await api.get('/api/parts/getPartsByBrand/' + brand);
 }
 
 export async function createCarBuyOutRequest(carData) {
-    return await api.post(host + '/api/carRequests', carData);
+    return await api.post('/api/carRequests', carData);
 }
 
 export async function getCarRequests(){
-    return await api.get(host + '/api/carRequests');
+    return await api.get('/api/carRequests');
 }
